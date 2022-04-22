f8604ec97075cda6dfb37e152240ce5271400b44
christopheralphonse <christopheralphonse96@gmail.com>
2022-04-21 20:49:47 -0400

    ➕

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-
-const App = () => (
-  <div className="App">
-    <h1> doe, jane</h1>
-    <p> age:{}</p>
-    <p> Hair Color:{}</p>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <p>Hello React</p>
+      <p> just words </p>
+    </div>
+  );
+};
 
 export default App;
